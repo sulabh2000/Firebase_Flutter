2b69b239ace849a0978d08d41c52dd768beec094
Sulabh Bansal <bansalsulabh13@gmail.com>
2020-09-06 13:39:14 +0530

    adding command feature

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 
 class MyChat extends StatefulWidget {
   @override
@@ -9,6 +11,14 @@ class MyChat extends StatefulWidget {
 
 class _MyChatState extends State<MyChat> {
   var authc = FirebaseAuth.instance;
+  var fsconnect = FirebaseFirestore.instance;
+  var command;
+  myweb(cmd) async {
+    var url = "http://192.168.43.33/cgi-bin/stop.py?x=${cmd}";
+    var response = await http.get(url);
+    return response.body;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +51,43 @@ class _MyChatState extends State<MyChat> {
           )
         ],
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          width: 400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                onChanged: (value) {
+                  command = value;
+                },
+                decoration: InputDecoration(
+                    hintText: "Type Command",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Material(
+                  color: Colors.lightBlueAccent,
+                  elevation: 10,
+                  borderRadius: BorderRadius.circular(10),
+                  child: MaterialButton(
+                    minWidth: 100,
+                    height: 40,
+                    onPressed: () async {
+                      var x = await myweb(command);
+                      fsconnect.collection("Students").add({
+                        "Output": x,
+                      });
+                    },
+                    child: Text("Run Command"),
+                  )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
